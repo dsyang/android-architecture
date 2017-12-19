4ae8527ef4de0fd24782bb2dcffe0deaef9f2e93
Dan Yang <dsyang92@gmail.com>
2017-12-19 13:25:15 -0800

    Replace recycler and use ViewRenderInfo

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragment.java
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragment.java
@@ -56,8 +56,6 @@ public class TasksFragment extends Fragment implements TasksContract.View {
 
     private TasksContract.Presenter mPresenter;
 
-    private TasksAdapter mListAdapter;
-
     private View mNoTasksView;
 
     private ImageView mNoTaskIcon;
@@ -70,6 +68,8 @@ public class TasksFragment extends Fragment implements TasksContract.View {
 
     private LithoView mFilteringLabelView;
 
+    private LithoView mTaskListView;
+
     public TasksFragment() {
         // Requires empty public constructor
     }
@@ -81,7 +81,6 @@ public class TasksFragment extends Fragment implements TasksContract.View {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mListAdapter = new TasksAdapter(new ArrayList<Task>(0), mItemListener);
     }
 
     @Override
@@ -107,8 +106,7 @@ public class TasksFragment extends Fragment implements TasksContract.View {
         View root = inflater.inflate(R.layout.tasks_frag, container, false);
 
         // Set up tasks view
-        ListView listView = (ListView) root.findViewById(R.id.tasks_list);
-        listView.setAdapter(mListAdapter);
+        mTaskListView = (LithoView) root.findViewById(R.id.tasks_list);
         mFilteringLabelView = (LithoView) root.findViewById(R.id.filteringLabel);
         mTasksView = (LinearLayout) root.findViewById(R.id.tasksLL);
 
@@ -145,7 +143,7 @@ public class TasksFragment extends Fragment implements TasksContract.View {
                 ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark)
         );
         // Set the scrolling view in the custom SwipeRefreshLayout.
-        swipeRefreshLayout.setScrollUpChild(listView);
+        swipeRefreshLayout.setScrollUpChild(mTaskListView);
 
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -246,7 +244,10 @@ public class TasksFragment extends Fragment implements TasksContract.View {
 
     @Override
     public void showTasks(List<Task> tasks) {
-        mListAdapter.replaceData(tasks);
+        mTaskListView.setComponent(Tasks.create(mTaskListView.getComponentContext())
+            .tasks(tasks)
+            .itemListener(mItemListener)
+            .build());
 
         mTasksView.setVisibility(View.VISIBLE);
         mNoTasksView.setVisibility(View.GONE);
@@ -359,87 +360,6 @@ public class TasksFragment extends Fragment implements TasksContract.View {
     @Override
     public boolean isActive() {
         return isAdded();
-    }
-
-    private static class TasksAdapter extends BaseAdapter {
-
-        private List<Task> mTasks;
-        private TaskItemListener mItemListener;
-
-        public TasksAdapter(List<Task> tasks, TaskItemListener itemListener) {
-            setList(tasks);
-            mItemListener = itemListener;
-        }
-
-        public void replaceData(List<Task> tasks) {
-            setList(tasks);
-            notifyDataSetChanged();
-        }
-
-        private void setList(List<Task> tasks) {
-            mTasks = checkNotNull(tasks);
-        }
-
-        @Override
-        public int getCount() {
-            return mTasks.size();
-        }
-
-        @Override
-        public Task getItem(int i) {
-            return mTasks.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            View rowView = view;
-            if (rowView == null) {
-                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
-                rowView = inflater.inflate(R.layout.task_item, viewGroup, false);
-            }
-
-            final Task task = getItem(i);
-
-            TextView titleTV = (TextView) rowView.findViewById(R.id.title);
-            titleTV.setText(task.getTitleForList());
-
-            CheckBox completeCB = (CheckBox) rowView.findViewById(R.id.complete);
-
-            // Active/completed task UI
-            completeCB.setChecked(task.isCompleted());
-            if (task.isCompleted()) {
-                rowView.setBackgroundDrawable(viewGroup.getContext()
-                        .getResources().getDrawable(R.drawable.list_completed_touch_feedback));
-            } else {
-                rowView.setBackgroundDrawable(viewGroup.getContext()
-                        .getResources().getDrawable(R.drawable.touch_feedback));
-            }
-
-            completeCB.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (!task.isCompleted()) {
-                        mItemListener.onCompleteTaskClick(task);
-                    } else {
-                        mItemListener.onActivateTaskClick(task);
-                    }
-                }
-            });
-
-            rowView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    mItemListener.onTaskClick(task);
-                }
-            });
-
-            return rowView;
-        }
     }
 
     public interface TaskItemListener {
---
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksSpec.java
@@ -0,0 +1,100 @@
+package com.example.android.architecture.blueprints.todoapp.tasks;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import com.example.android.architecture.blueprints.todoapp.R;
+import com.example.android.architecture.blueprints.todoapp.data.Task;
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.ComponentLayout;
+import com.facebook.litho.annotations.*;
+import com.facebook.litho.sections.SectionContext;
+import com.facebook.litho.sections.common.DataDiffSection;
+import com.facebook.litho.sections.common.RenderEvent;
+import com.facebook.litho.sections.widget.RecyclerCollectionComponent;
+import com.facebook.litho.viewcompat.SimpleViewBinder;
+import com.facebook.litho.viewcompat.ViewBinder;
+import com.facebook.litho.viewcompat.ViewCreator;
+import com.facebook.litho.widget.ComponentRenderInfo;
+import com.facebook.litho.widget.RenderInfo;
+import com.facebook.litho.widget.ViewRenderInfo;
+
+import java.util.List;
+
+@LayoutSpec
+public class TasksSpec {
+
+    private static final ViewCreator TASK_ITEM_VIEW_CREATOR = new ViewCreator() {
+        @Override
+        public View createView(Context c) {
+            LayoutInflater inflater = LayoutInflater.from(c);
+            return inflater.inflate(R.layout.task_item, null, false);
+        }
+    };
+
+    @OnCreateLayout
+    static ComponentLayout onCreateLayout(
+        ComponentContext c,
+        @Prop List<Task> tasks) {
+        return RecyclerCollectionComponent.create(c)
+            .disablePTR(true)
+            .heightPercent(100)
+            .section(DataDiffSection.<Task>create(new SectionContext(c))
+                .data(tasks)
+                .renderEventHandler(Tasks.renderTask(c))
+                .build())
+            .buildWithLayout();
+    }
+
+    @OnEvent(RenderEvent.class)
+    static RenderInfo renderTask(
+        final ComponentContext c,
+        @FromEvent int index,
+        @FromEvent final Task model,
+        @Prop final TasksFragment.TaskItemListener itemListener) {
+        return ViewRenderInfo.create()
+            .viewCreator(TASK_ITEM_VIEW_CREATOR)
+            .viewBinder(new SimpleViewBinder() {
+
+                @Override
+                public void bind(View rowView) {
+                    TextView titleTV = (TextView) rowView.findViewById(R.id.title);
+                    titleTV.setText(model.getTitleForList());
+
+                    CheckBox completeCB = (CheckBox) rowView.findViewById(R.id.complete);
+
+                    // Active/completed task UI
+                    completeCB.setChecked(model.isCompleted());
+                    if (model.isCompleted()) {
+                        rowView.setBackgroundDrawable(
+                            c.getResources().getDrawable(R.drawable.list_completed_touch_feedback));
+                    } else {
+                        rowView.setBackgroundDrawable(
+                            c.getResources().getDrawable(R.drawable.touch_feedback));
+                    }
+
+                    completeCB.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (!model.isCompleted()) {
+                                itemListener.onCompleteTaskClick(model);
+                            } else {
+                                itemListener.onActivateTaskClick(model);
+                            }
+                        }
+                    });
+
+                    rowView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            itemListener.onTaskClick(model);
+                        }
+                    });
+                }
+            })
+            .build();
+    }
+}
